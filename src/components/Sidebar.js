c609509975bc738e7067ebe6626ab176414fda83
Dudley-hue <dudleykimani331@gmail.com>
2024-08-17 23:41:20 +0300

    addition

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,10 +7,10 @@ const Sidebar = ({ isAdmin }) => (
     
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/cohorts">Cohorts</Link></li>
-        {isAdmin && <li><Link to="/admin">Admin</Link></li>}
-        <li><Link to="/student">Student</Link></li>
+        <li><Link to="/"><i class="fa-solid fa-suitcase fa-flip"></i>Home</Link></li>
+        <li><Link to="/cohorts"><i class="fa-solid fa-users-rays fa-flip"></i>Cohorts</Link></li>
+        {isAdmin && <li><Link to="/admin"><i class="fa-solid fa-user-secret fa-flip"></i>Admin</Link></li>}
+        <li><Link to="/student"><i class="fa-solid fa-users fa-flip"></i>Student</Link></li>
       </ul>
     </nav>
   </div>
